1b8a318762516a8b5de6be47e1196dbc2fc57c52
Salman Khurshid <salmankhurshid.me@gmail.com>
2025-10-29 04:06:19 +0530

    Assignment for part2a from 2.1 to 2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,22 @@
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const Course = ({ courses }) => {
+  return (
+    <>
+      {courses.map((course) => {
+        const total = course.parts.reduce((a, c) => a + c.exercises, 0);
+        return (
+          <div key={course.id}>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total total={total} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Course;
---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,3 @@
+const Total = (props) => <h4>total of {props.total} exercises</h4>;
+
+export default Total;
