a87ea5c940ed176b070dd85adbea0e955abe7873
Salman Khurshid <salmankhurshid.me@gmail.com>
2025-11-14 02:03:28 +0530

    Assignment for chapter 4 for exercise 4.1 and 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const blogRouter = require("./controller/blogs");
+
+const app = express();
+
+mongoose.connect(config.MONGODB_URI, { family: 4 }).then(() => {
+  console.log("Database connected...");
+});
+
+app.use(express.json());
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
