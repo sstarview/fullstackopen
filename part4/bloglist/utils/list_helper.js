1be38bdf804bc84ac6c44484db8c33ce4f06c980
Salman Khurshid <salmankhurshid.me@gmail.com>
2025-11-14 03:45:01 +0530

    Assignment for chapter 4 for exercise 4.4 to 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogPosts) => {
+  const reducer = (sum, current) => {
+    return (sum += current.likes);
+  };
+  return blogPosts.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogPosts) => {
+  if (blogPosts.length === 0) return null;
+  return blogPosts.reduce((favorite, current) =>
+    favorite.likes < current.likes ? current : favorite
+  );
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
