93f0a335fc7bd3fa2b08e83c22104fc9c499cc34
Salman Khurshid <salmankhurshid.me@gmail.com>
2025-11-14 20:32:15 +0530

    Assignment for part 4 for exercise 4.6 to 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -14,6 +16,38 @@ const favoriteBlog = (blogPosts) => {
   return blogPosts.reduce((favorite, current) =>
     favorite.likes < current.likes ? current : favorite
   );
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  const result = _.chain(blogs)
+    .countBy("author")
+    .map((count, author) => ({
+      author: author,
+      blog: count,
+    }))
+    .maxBy("blog")
+    .value();
+
+  return result;
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  const result = _.chain(blogs)
+    .groupBy("author")
+    .map((authorBlogs, authorName) => ({
+      author: authorName,
+      likes: _.sumBy(authorBlogs, "likes"),
+    }))
+    .maxBy("likes")
+    .value();
+
+  return result;
 };
 
 module.exports = {
